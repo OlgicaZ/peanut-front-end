2eafe69e1ba659f010f350a4eab1c3b7406f2a88
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2023-11-23 11:06:24 -0500

    create folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.scss';
+import './App.scss'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={Home} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.scss';
+
+import homeImg from './../../assets/images/picture1.png';
+
+function Home() {
+    return (
+        <>
+        Hi
+        <img src={homeImg} />
+        </>
+    );
+}
+
+export default Home;
