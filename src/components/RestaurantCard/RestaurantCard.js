368ec76c042f57a06f5d9a523c954162c761990d
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2023-11-24 13:14:20 -0500

    link the selection of a restaurant card to the appropriate page

--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -1,14 +1,22 @@
-import './RestaurantCard.scss'
+import './RestaurantCard.scss';
+
+import { useNavigate } from 'react-router-dom';
 
 function RestaurantCard( {restaurant} ) {
 
+    const id = restaurant.id;
     const name = restaurant.restaurant_name;
     const description = restaurant.description;
     const imageUrl = restaurant.image_url;
 
+    const navigate = useNavigate();
+
+    const handleRestaurantSelection = () => {
+        navigate(`/restaurant-details/${id}`);
+    }
 
     return (        
-        <div className="restaurant-card">
+        <div className="restaurant-card" onClick={handleRestaurantSelection}>
             <div className="restaurant-card__image-container">
                 <img className="restaurant-card__image" src={imageUrl}  alt={name} />
             </div>
