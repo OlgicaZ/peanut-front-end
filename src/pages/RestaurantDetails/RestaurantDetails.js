c14d6fefee8b12785f15d3af617e4786d26a8c7a
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2023-11-25 13:41:13 -0500

    create a day mapping function

--- src/pages/RestaurantDetails/RestaurantDetails.js
+++ src/pages/RestaurantDetails/RestaurantDetails.js
@@ -1,7 +1,64 @@
 import './RestaurantDetails.scss';
 
-function RestaurantDetails() {
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
+function RestaurantDetails() {
+    const { id } = useParams();
+
+    const [currentRestaurant, setCurrentRestaurant] = useState(null);
+    const [businessHours, setBusinessHours] = useState(null);
+
+    useEffect(() => {
+
+        const fetchRestaurant = async () => {
+            try {
+                const restaurantDetails = await axios.get(`http://localhost:8080/api/restaurants/${id}`);
+                setCurrentRestaurant(restaurantDetails.data[0]);
+
+                const businessHours = await axios.get(`http://localhost:8080/api/restaurants/${id}/business-hours`);
+                setBusinessHours(businessHours.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchRestaurant();
+    }, []);
+
+    if (!currentRestaurant || !businessHours) {
+        return <div>Loading!</div>
+    }
+
+    return (
+        <main>
+            <div>
+                <img src={currentRestaurant.image_url} alt={currentRestaurant.description} />
+            </div>
+            <div>
+                <section>
+                    <div>
+                        <h1>{currentRestaurant.restaurant_name}</h1>
+                        <h2>{currentRestaurant.cuisine} Restaurant</h2>
+                    </div>
+                    <div>View Menu</div>
+                </section>
+                <section>
+                    <h2>About</h2>
+                    <p>{currentRestaurant.about}</p>
+                </section>
+                <section>
+                    <h2>Work Hours</h2>
+                    <ul>
+                        {
+                            businessHours.map((day) => <li>{day.day} : {day.open_time} - {day.close_time} </li>)
+                        }
+                    </ul>
+                </section>
+            </div>
+        </main>
+    );
 }
 
 export default RestaurantDetails
