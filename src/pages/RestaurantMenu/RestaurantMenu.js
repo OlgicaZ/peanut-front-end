0eb61c98086d310cb33ef19f77af5d4452a5902e
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2023-11-25 15:39:55 -0500

    add allergens/dietary-restrictions buttons

--- src/pages/RestaurantMenu/RestaurantMenu.js
+++ src/pages/RestaurantMenu/RestaurantMenu.js
@@ -1,9 +1,58 @@
 import './RestaurantMenu.scss'
 
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function RestaurantMenu() {
+
+    const { id } = useParams();
+
+    const [menuItems, setMenuItems] = useState(null);
+
+    useState(() => {
+        const fetchMenuItems = async () => {
+            const response = await axios.get(`http://localhost:8080/api/restaurants/${id}/menu-items`);
+            setMenuItems(response.data);
+        }
+
+        fetchMenuItems();
+    }, [])
+
+    if (!menuItems) {
+        return <div>Loading!</div>
+    }
+
     return (
         <main>
-
+            <h1>{menuItems[0].restaurant_name}</h1>
+            <section>
+                <section>
+                    <div>
+                        <h2>Allergens</h2>
+                    </div>
+                    <div>
+                        <div>Diary</div>
+                        <div>Gluten</div>
+                        <div>Shellfish</div>
+                    </div>
+                    <div>
+                        <div>Tree Nuts</div>
+                        <div>Peanuts</div>
+                        <div>Fish</div>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h2>Dietary Restrictions</h2>
+                    </div>
+                    <div>
+                        <div>Vegan</div>
+                        <div>Vegetarian</div>
+                        <div>Pescatarian</div>
+                    </div>
+                </section>
+            </section>
         </main>
     );
 }
